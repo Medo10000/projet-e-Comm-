ff0536a3b03bd40d2ea7b1a0197fc6a11d541b16
Mohamed Said <merguibi10@gmail.com>
2021-10-15 13:57:01 +0100

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,21 +4,17 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto">
-            <form>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <form action='login' method="POST">
+                @csrf
+                <div class="mb-3 form-group">
+                  <label for="email" class="form-label">Adresse Email</label>
+                  <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                <div class="mb-3 form-group">
+                  <label for="password" class="form-label">Mot de passe</label>
+                  <input type="password" class="form-control" name="password" id="password">
                 </div>
-                <div class="mb-3 form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">S'identifier</button>
               </form>
         </div>
     </div>
